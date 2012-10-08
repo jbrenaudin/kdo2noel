b30dc9a424725f8b89ffe0c9063f221b6f9698db
jb <jb@10.0.1.6>
2012-10-08 22:37:06 +0200

    Pareil

--- app/common/FormatMontant.java
+++ app/common/FormatMontant.java
@@ -1,11 +1,15 @@
 package common;
 
 import java.text.DecimalFormat;
+import java.util.Locale;
+
 import play.templates.JavaExtensions;
 
 public class FormatMontant extends JavaExtensions {
 
 	public static String enEuros(Number montant) {
-		return new DecimalFormat("#,##0.00' €'").format(montant);
+		DecimalFormat format = (DecimalFormat) DecimalFormat.getInstance(Locale.FRENCH);
+		format.applyPattern("###,###.00' €'");
+		return format.format(montant);
 	}
 }
